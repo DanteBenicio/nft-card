53ac62cd84eebf0eb2e3e39ee218dad60f56f8f5
DanteBenicio <dantecosta79@gmail.com>
2022-03-19 21:39:25 -0300

    add body event listener

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
 const imageWrapper = document.getElementById('nft_image').parentElement;
 const image = document.getElementById('nft_image')
+const body = document.querySelector('body')
 
 imageWrapper.addEventListener('click', e => {
   image.classList.toggle('clicked')
+  console.log(e.currentTarget)
+})
+
+body.addEventListener('click', e => {
+  if (e.target !== imageWrapper) {
+    image.classList.remove('clicked')
+  }
 })
